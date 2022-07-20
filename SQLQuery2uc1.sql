0de8f2dfbbec024c009e9ffb967ba57a1850719f
Yogesh <yogeshbietjhs@gmail.com>
2022-07-20 16:27:11 +0530

    Table is created

--- SQLQuery2uc1.sql
+++ SQLQuery2uc1.sql
@@ -1,3 +1,14 @@
 		--uc1
-create database payroll_service
-use payroll_service
+--create database payroll_service
+--use payroll_service
+
+    --uc2
+
+CREATE TABLE Persons (
+    PersonID int,
+    name varchar(255),
+    Salary varchar(255),
+    start DATE NOT Null,
+    primary key (personID)
+);
+select * from Persons
